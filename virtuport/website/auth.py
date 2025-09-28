b35d7dedc628ef8c406d756fdfe3c1da0ce3ab9e
Akul Sharma <akul.s2006@gmail.com>
2025-09-28 15:20:44 +0530

    Enhance HTML templates for accessibility and styling
    - Added accessibility stylesheets to industry partnership, login, news and events, programs, research and innovation, signup, and social media integration pages.
    - Improved HTML structure by ensuring proper indentation and formatting for better readability.
    - Updated navigation and footer sections across multiple templates for consistency.
    - Enhanced content presentation in main sections, including better formatting of text and lists for improved user experience.

---
+++ virtuport/website/auth.py
@@ -0,0 +1,76 @@
+from flask import Blueprint, render_template, request, flash
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/Home')
+def home():
+    return render_template("Home.html")
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    data = request.form
+    print(data)
+    return render_template("login.html", boolean=True)
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        data = request.form
+        print(data)
+
+@auth.route('/about')
+def about():
+    return render_template("about.html")
+
+@auth.route('accessibility')
+def accessibility():
+    return render_template("accessibility.html")
+
+@auth.route('/admissions')
+def admissions():
+    return render_template("admissions.html")
+
+@auth.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@auth.route('/digital_learning')
+def digital_learning():
+    return render_template("Digital Learning.html")
+
+@auth.route('/faculty_resources')
+def faculty_resources():
+    return render_template("faculty and staff resources.html")
+
+@auth.route('/curriculum')
+def curriculum():
+    return render_template("Hands-on Curriculum.html")
+
+@auth.route('/index')
+def index():
+    return render_template("index.html")
+
+@auth.route('/industry_partnership')
+def industry_partnership():
+    return render_template("industry-partnership.html")
+
+@auth.route('/news_events')
+def news_events():
+    return render_template("news and event.html")
+
+@auth.route('/programs')
+def programs():
+    return render_template("programs.html")
+
+@auth.route('/research')
+def research():
+    return render_template("research and innovation.html")
+
+@auth.route('/social_media')
+def social_media():
+    return render_template("social media integration.html")
+
+@auth.route('/student_resources')
+def student_resources():
+    return render_template("Student Resources.html")
